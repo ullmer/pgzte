3ca559bd31054d8d25e72f2bdef38c1336553b91
Brygg Ullmer <bullmer@clemson.edu>
2026-07-04 16:48:57 -0400

    stub

---
+++ src/pgzte.py
@@ -0,0 +1,7 @@
+# pgzte is a fork of Pygame Turbo, which is a more swiftly-evolving, 
+# pygame-ce compliant fork of Pygame Zero, the zero-boilerplate games programming framework.
+
+# pgzte is initially agnostic between Pygame Turbo and Zero, offering a preferential setting, 
+# with fallback; and agnostic to the incorporation of additional Enodia features.
+
+### end ###
