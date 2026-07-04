9c3152c7479b44a98e8a3e2661babc191d9b454a
Brygg Ullmer <bullmer@clemson.edu>
2026-07-04 17:10:12 -0400

    added verbosity flag, defaulting to verbose

--- src/pgzte.py
+++ src/pgzte.py
@@ -8,20 +8,22 @@
 # Begun 2026-07-04
 
 # This should probably evolve, but for starters:
+
 preferPgxVariantPrimary   = "pgturbo"
 preferPgxVariantSecondary = "pgzero"
+verbose = True
 
 try:
   if preferPgxVariantPrimary == "pgturbo": import pgturbo
   if preferPgxVariantPrimary == "pgzero":  import pgzero
-  print("pgzte: preferred primary pgx variant", preferPgxVariantPrimary, "import succeeds")
+  if verbose: print("pgzte: preferred primary pgx variant", preferPgxVariantPrimary, "import succeeds")
 except:
   print("pgzte: preferred primary pgx variant", preferPgxVariantPrimary, "import fails; trying secondary")
 
   try:
     if preferPgxVariantSecondary == "pgturbo": import pgturbo
     if preferPgxVariantSecondary == "pgzero":  import pgzero
-    print("pgzte: secondary primary pgx variant", preferPgxVariantSecondary, "import succeeds")
+    if verbose: print("pgzte: secondary primary pgx variant", preferPgxVariantSecondary, "import succeeds")
   except:
     print("pgzte: preferred secondary pgx variant", preferPgxVariantSecondary, "import fails.")
 
