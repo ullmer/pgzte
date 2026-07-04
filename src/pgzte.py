9603ee02206ffe469c35696734e42b009eb585f4
Brygg Ullmer <bullmer@clemson.edu>
2026-07-04 16:56:21 -0400

    first warmup, not yet tested

--- src/pgzte.py
+++ src/pgzte.py
@@ -4,4 +4,22 @@
 # pgzte is initially agnostic between Pygame Turbo and Zero, offering a preferential setting, 
 # with fallback; and agnostic to the incorporation of additional Enodia features.
 
+# This should probably evolve, but for starters:
+preferPgxVariantPrimary   = "pgturbo"
+preferPgxVariantSecondary = "pgzero"
+
+try:
+  if preferPgxVariantPrimary is "pgturbo": import pgturbo
+  if preferPgxVariantPrimary is "pgzero":  import pgzero
+  print("pgzte: preferred primary pgx variant import succeeds")
+except:
+  print("pgzte: preferred primary pgx variant", preferPgxVariantPrimary, " import fails; trying secondary"
+
+  try:
+    if preferPgxVariantSecondary is "pgturbo": import pgturbo
+    if preferPgxVariantSecondary is "pgzero":  import pgzero
+    print("pgzte: secondary primary pgx variant import succeeds")
+  except:
+    print("pgzte: preferred secondary pgx variant", preferPgxVariantSecondary, " import fails.")
+
 ### end ###
